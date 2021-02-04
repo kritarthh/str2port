33261fef6bfa22d8c4d89e62936b0694cf1c5dc5
Kritarth <me@kritarth.net>
2021-02-04 15:16:15 +0530

    Update __main__.py

--- str2port/__main__.py
+++ str2port/__main__.py
@@ -6,8 +6,10 @@ from . import str2port
 @click.command()
 @click.argument('string')
 @click.option('--use-iana', is_flag=True, help='Exclude used ports from IANA list (default: false)')
-def cli(string, use_iana):
-    click.echo(' '.join(str(i) for i in str2port(string, use_iana=use_iana)))
+@click.option('--start', default=1024)
+@click.option('--end', default=65536)
+def cli(string, use_iana, start, end):
+    click.echo(' '.join(str(i) for i in str2port(string, use_iana, start, end)))
 
 
 if __name__ == '__main__':
